e06d209408feefc001a3941bed6e5a05c403b1b5
haritiana adriano <hei.adriano.4@gmail.com>
2022-09-15 20:10:42 +0200

    feat: table for hotel

--- Front-end/front-end-hotel-room/src/components/pages/hotel-page/HotelPage.js
+++ Front-end/front-end-hotel-room/src/components/pages/hotel-page/HotelPage.js
@@ -1,8 +1,31 @@
 import NavBar from "../../controller/Navbar";
 import { motion } from "framer-motion";
+import Table from 'react-bootstrap/Table';
+import axios from "axios";
+import "./Hotel.css"
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 function HotelPage() {
+    //variable sector
+    const [hotel, setHotel] = useState([]);
+
+
+    const getHotelData = async () => {
+        try{
+            const data = await axios.get("http://localhost:8080/hotels");
+            console.log(data.data);
+            setHotel(data.data);
+        }catch (error){
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getHotelData()
+    });
+
     return (
         <motion.div
             initial={{width: 0}}
@@ -10,7 +33,31 @@ function HotelPage() {
             exit={{x: window.innerWidth}}
         >
             <NavBar />
-            Hotel
+            <h2>Hotels</h2>
+            <div>
+            <Table striped bordered hover>
+                <thead className="th">
+                    <tr>
+                    <th>#</th>
+                    <th>Localisation</th>
+                    <th>Type Hotel</th>
+                    <th>Name Hotel</th>
+                    </tr>
+                </thead>
+                <tbody className="tb">
+                {hotel.map((item)  => {
+                    return(
+        <tr>
+          <td>{item.idHotel}</td>
+          <td>{item.location}</td>
+          <td>{item.typeHotel.hotelType}</td>
+          <td>{item.nameHotel}</td>
+        </tr>
+                    )
+                    })}
+                    </tbody>
+                </Table>
+            </div>
         </motion.div>
     )
 }
